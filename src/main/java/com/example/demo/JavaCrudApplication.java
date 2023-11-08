579ee0b78799788a7d6b703c7ef2027147ced27a
GeisslerRua2050 <145591045+GeisslerRua2050@users.noreply.github.com>
2023-11-08 10:55:52 -0500

    Add files via upload

---
+++ src/main/java/com/example/demo/JavaCrudApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCrudApplication.class, args);
+	}
+
+}
+
+@GetMapping("/")
+public String init() {
+	return String.format("Welcome to Java CRUD Application!");
+}
+
+@GetMapping("/hello")
+public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+return String.format("Hello %s!", name);
+}
